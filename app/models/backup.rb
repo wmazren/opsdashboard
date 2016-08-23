d67f623a8f01019c70ba93647489ee6a06887c24
wmazren <w.m.azren@gmail.com>
2016-08-23 21:10:42 +0800

    add seeds data

--- app/models/backup.rb
+++ app/models/backup.rb
@@ -1,2 +1,12 @@
 class Backup < ActiveRecord::Base
+  require 'csv'
+
+  def self.import_data
+    data = []
+    CSV.foreach('public/sample.csv', headers: true) do |row|
+      data << {server_name: row['ServerName'], date_time: row['DateTime'], job_name: row['JobName'], event_id: row['EventId'],
+        status: row['Status']}
+    end
+    Backup.create(data)
+  end
 end
